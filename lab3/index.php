fe138f0f7b77dd3300f3193b730df98d943e9221
Sancho145 <alv.petrov@rambler.ru>
2026-01-16 12:59:45 +0300

    php 1

--- lab3/index.php
+++ lab3/index.php
@@ -1,3 +1,30 @@
-<php
-echo "танк";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>57</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="text" name="Boom" id="">
+        <button type="submit">Egor</button>
+</form>
+    
+</body>
+</html>
+<?php
+if (isset($_GET['Boom'])){
+    $egor_lox = explode(' ', $_GET['Boom']);
+ upFunc($egor_lox);
+ echo implode(' ', $egor_lox);   
+}
+function upFunc(&$egor_lox){
+    for($i=0; $i< count($egor_lox);$i++){
+        if(($i % 2)> 0){
+            $egor_lox[$i] = strtoupper($egor_lox[$i]);
+        }
+    }
+    
+}
+
